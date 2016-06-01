3474a0200c43feb0551db2f64c7add40f4a8f710
kof97 <1048434786@qq.com>
2016-06-01 19:43:08 +0800

    部署mongodb

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,70 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+/**
+ * register view.
+ * 
+ * 
+ */
+router.get('/reg', function(req, res) {
+    res.render('reg', { title: '用户注册' });
+});
+
+/**
+ * register operating.
+ * 
+ * 
+ */
+router.post('/reg', function(req, res) {
+
+});
+
+/**
+ * login view.
+ * 
+ * 
+ */
+router.get('/login', function(req, res) {
+    res.render('login', { title: '用户登录' });
+});
+
+/**
+ * login operating.
+ * 
+ * 
+ */
+router.post('/login', function(req, res) {
+
+});
+
+/**
+ * post article view.
+ * 
+ * 
+ */
+router.get('/post', function (req, res) {
+    res.render('post', { title: '发表文章' });
+});
+
+/**
+ * post article operating.
+ * 
+ * 
+ */
+router.post('/post', function (req, res) {
+
+});
+
+/**
+ * logout.
+ * 
+ * 
+ */
+router.get('/logout', function (req, res) {
+
 });
 
 module.exports = router;
