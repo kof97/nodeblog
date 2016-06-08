c5015bd10555831adb0628d12a70a99690e4489f
kof97 <1048434786@qq.com>
2016-06-08 23:07:13 +0800

    添加gulp browserify

---
+++ src/javascripts/main.js
@@ -0,0 +1,9 @@
+var angular = require("angular");
+
+require("angular-route");
+require("angular-cookies");
+require("angular-resource");
+require("angular-sanitize");
+
+require("./services");
+require("./app");
